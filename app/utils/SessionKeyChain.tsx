5a16e99c3cb73459c7914fcfc638199a612e6adc
devinluize <devin.luize12@gmail.com>
2024-12-01 16:04:10 +0700

    update expo module

---
+++ app/utils/SessionKeyChain.tsx
@@ -0,0 +1,30 @@
+import * as SecureStore from 'expo-secure-store';
+
+export const saveSecureItem = async (key: string, value: string): Promise<void> => {
+localStorage.setItem(key, value);
+    // try {
+    //     await SecureStore.setItemAsync(key, value, {
+    //         keychainAccessible: SecureStore.WHEN_UNLOCKED,
+    //     });
+    // } catch (error) {
+    //     console.error(`Error saving ${key}:`, error);
+    // }
+};
+
+export const getSecureItem = async (key: string): Promise<string | null> => {
+return await   localStorage.getItem(key)
+    // try {
+    //     return await SecureStore.getItemAsync(key);
+    // } catch (error) {
+    //     console.error(`Error retrieving ${key}:`, error);
+    //     return null;
+    // }
+};
+
+export const deleteSecureItem = async (key: string): Promise<void> => {
+    try {
+        await SecureStore.deleteItemAsync(key);
+    } catch (error) {
+        console.error(`Error deleting ${key}:`, error);
+    }
+};
